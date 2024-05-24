b42c41388078ad8274eb8d3c6b60ba027cd81d97
Raunak49 <1000015951@dit.edu.in>
2024-05-24 21:29:06 +0530

    possible moves

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,18 +7,40 @@ const chess = new Chess();
 function App() {
   const [selectedSquare, setSelectedSquare] = useState(null);
   const [position, setPosition] = useState(chess.fen());
+  const [optionSquares, setOptionSquares] = useState({});
 
   const checkDrop = (from, to, promotion) => {
     try {
       if (promotion) {
         promotion = promotion.substring(1).toLowerCase();
       }
-      console.log(chess.move({ from, to, promotion }));
+      chess.move({ from, to, promotion });
       setPosition(chess.fen());
     } catch (error) {
-      console.log(error);
       return null;
+    } finally {
+      setOptionSquares({});
     }
+  };
+
+  const highlightSquares = (square) => {
+    const moves = chess.moves({ square, verbose: true});
+    const newSquares = {};
+    moves.map((move) => {
+      newSquares[move.to] = {
+        background:
+          chess.get(move.to) &&
+          chess.get(move.to).color !== chess.get(square).color
+            ? "radial-gradient(circle, rgba(0,0,0,.1) 85%, transparent 85%)"
+            : "radial-gradient(circle, rgba(0,0,0,.1) 25%, transparent 25%)",
+        borderRadius: "50%",
+      };
+      return move;
+    });
+    newSquares[square] = {
+      background: "rgba(255, 255, 0, 0.4)",
+    };
+    setOptionSquares(newSquares);
   };
 
   const squareClick = (square) => {
@@ -28,8 +50,7 @@ function App() {
       return;
     }
     setSelectedSquare(square);
-    const highlightSquares = chess.moves({ square, verbose: true}).map((move) => move.to);
-    console.log(highlightSquares);
+    highlightSquares(square);
   };
 
   return (
@@ -44,6 +65,8 @@ function App() {
             showBoardNotation={true}
             animationDuration={0}
             onSquareClick={squareClick}
+            customSquareStyles={optionSquares}
+            onPieceDragBegin={(piece, square) => highlightSquares(square)}
           />
         </div>
       </header>
