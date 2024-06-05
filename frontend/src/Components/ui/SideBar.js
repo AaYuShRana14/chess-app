b723f4667e6b37646f6e28e0b434ed71a7deb13c
AaYuShRana14 <antineutrino1464@gmail.com>
2024-06-05 16:20:42 +0530

    history rendered on front end

--- frontend/src/Components/ui/SideBar.js
+++ frontend/src/Components/ui/SideBar.js
@@ -134,7 +134,6 @@ const History = () => {
           }
         });
         setHistoryData(res.data);
-        console.log(res.data);
       } catch (err) {
         setError(err);
       }
@@ -147,18 +146,20 @@ const History = () => {
 
   return (
     <div className="history">
-      <div className="history-header">
-        <p>Sl.No</p>
+      <div className="history-header">   
+      <p>S.no</p>
         <p>White</p>
         <p>Black</p>
+        <p>Winner</p>
       </div>
       <div className="history-content">
         {historyData.length > 0 ? (
           historyData.map((game, index) => (
             <div key={index} className="history-item">
               <p>{index + 1}</p>
-              <p>{game.whitePlayer}</p>
-              <p>{game.blackPlayer}</p>
+              <p>{game.white}</p>
+              <p>{game.black}</p>
+              <p>{game.winner}</p>
             </div>
           ))
         ) : (
