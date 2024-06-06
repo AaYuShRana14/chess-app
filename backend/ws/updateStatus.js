34566c1b5698901cc2d92051fd740e5c41231b6f
AaYuShRana14 <antineutrino1464@gmail.com>
2024-06-06 16:44:12 +0530

    test

--- backend/ws/updateStatus.js
+++ backend/ws/updateStatus.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
 const updateStatus = async (whiteid,blackid,winnerid,verdict,moves,passkey) => {
+  console.log(whiteid,blackid,winnerid,verdict,moves,passkey);
   try {
     const response = await axios.put('https://chess-app-opin.onrender.com/game/update', {
         whiteid,
